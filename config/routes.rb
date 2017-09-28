d4f8a8c887e4f08e77ccdb133de76b5b0e0d38e7
Sabine <sabnimtz@outlook.com>
2017-09-28 16:55:20 -0400

    added resources states

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  
-  resources :cities, except: [:new, :edit]
+
+  scope :api, defaults: {format: :json} do
+	resources :cities, except: [:new, :edit]
+	resources :states, except: [:new, :edit]
+  end
+
   get '/ui' => 'ui#index'
   get '/ui#' => 'ui#index'
   root "ui#index"
