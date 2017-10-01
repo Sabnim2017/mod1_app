4e24fdd7b9a8780a31b33f0eb3d4aff64b9f11ff
Sabine <sabnimtz@outlook.com>
2017-10-01 08:23:38 -0400

    added resources state

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,7 @@ module Mod1App
     #to rails generate cmd line to be specific
     config.generators {|g| g.orm :active_record}
     #config.generators {|g| g.orm :mongoid}
+    
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
